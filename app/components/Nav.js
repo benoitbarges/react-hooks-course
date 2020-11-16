af37424b4ff5f5b2ff846377dbc51cdb5ac83cc9
benoit barges <benoitbarges@gmail.com>
2020-11-16 15:57:52 +0100

    refactorign theme with cotnext hook

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,44 +1,43 @@
 import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 import { NavLink } from 'react-router-dom'
+import { ThemeProvider } from '../contexts/theme'
 
 const activeStyle = {
   color: 'rgb(187, 46, 31)'
 }
 
 export default function Nav () {
+  const { theme, toggleTheme } = React.useContext(ThemeContext)
+
   return (
-    <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
-        <nav className='row space-between'>
-          <ul className='row nav'>
-            <li>
-              <NavLink
-                to='/'
-                exact
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/battle'
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{fontSize: 30}}
-            className='btn-clear'
-            onClick={toggleTheme}
-          >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className='row space-between'>
+      <ul className='row nav'>
+        <li>
+          <NavLink
+            to='/'
+            exact
+            activeStyle={activeStyle}
+            className='nav-link'>
+              Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/battle'
+            activeStyle={activeStyle}
+            className='nav-link'>
+              Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{fontSize: 30}}
+        className='btn-clear'
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
   )
 }
