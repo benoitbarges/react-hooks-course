8532c755c749363fa4425c37f8e13855720ddac3
benoit barges <benoitbarges@gmail.com>
2020-11-16 18:38:25 +0100

    refacto loading

--- app/hooks/useHover.js
+++ app/hooks/useHover.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function useHover() {
+export default function useHover () {
   const [hovering, setHovering] = React.useState(false)
 
   const onMouseOver = () => setHovering(true)
