47d292f795df14daaf338bf6090fb157f327a688
benoit barges <benoitbarges@gmail.com>
2020-11-16 17:36:03 +0100

    refacto hover

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Hover from './Hover'
+import useHover from './Hover'
 
 const styles = {
   container: {
@@ -25,15 +25,13 @@ const styles = {
 }
 
 export default function Tooltip ({ text, children }) {
+  const [hovering, attr] = useHover()
+
   return (
-    <Hover>
-      {(hovering) => (
-        <div style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-        </div>
-      )}
-    </Hover>
+    <div style={styles.container} {...attr}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
   )
 }
 
